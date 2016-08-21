09a617bb142cf9f783ccad8d12e7e4839ae5fd56
ZLinuxFan <ZLinuxFan@gmail.com>
2016-08-21 16:15:55 +0300

    Added test-client for SaveNameAndSurname servlet

--- src/clientTest/SaveNameAndSurnameTest.java
+++ src/clientTest/SaveNameAndSurnameTest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 
@@ -13,22 +14,63 @@ import java.net.URL;
 public class SaveNameAndSurnameTest {
 
     public static void main(String[] args) throws IOException {
-        doRequest("http://localhost:8080/info?name=serj1&surname=nimble1");
+        testRightUrl(3);
+        testNullPapametr(3);
+        testNullPapametrs(3);
     }
 
-    private static boolean doRequest(String urlToRead) throws IOException {
-        String response = "";
-        String line;
-        URL url = new URL(urlToRead);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-        urlConnection.setRequestMethod("GET");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-        while ((line = reader.readLine()) != null) {
-            response += line;
+    private static void testNullPapametrs(int testNum) {
+        for (int threadNum = 0; threadNum < 10; threadNum ++) {
+            Runnable runnable = new doRequest("http://localhost:8080/info?name&surname");
+            Thread thread = new Thread(runnable);
+            thread.run();
         }
-        System.out.println(response);
-        return true;
+    }
+
+    private static void testNullPapametr(int testNum) {
+        for (int threadNum = 0; threadNum < testNum; threadNum ++) {
+            Runnable runnable = new doRequest("http://localhost:8080/info?name=&surname=nimble" + threadNum);
+            Thread thread = new Thread(runnable);
+            thread.run();
+        }
+    }
+
+    private static void testRightUrl(int testNum) {
+        for (int threadNum = 0; threadNum < testNum; threadNum ++) {
+            Runnable runnable = new doRequest("http://localhost:8080/info?name=serj" + threadNum + "&surname=nimble" + threadNum);
+            Thread thread = new Thread(runnable);
+            thread.run();
+        }
+    }
+
+    private static class doRequest implements Runnable {
+        private String urlToRead;
+
+        public doRequest(String urlToRead) {
+            this.urlToRead = urlToRead;
+        }
+
+        @Override
+        public void run() {
+            String line;
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+                url = new URL(urlToRead);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
